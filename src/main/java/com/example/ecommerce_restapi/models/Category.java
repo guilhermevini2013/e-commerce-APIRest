d00daa62b59994dceacc2165c43798121243183f
01nevida <01@vida>
2023-10-14 17:28:28 -0300

    Initial commit

---
+++ src/main/java/com/example/ecommerce_restapi/models/Category.java
@@ -0,0 +1,2 @@
+package com.example.ecommerce_restapi.models;public class Product {
+}
---
+++ src/test/java/com/example/ecommerce_restapi/ECommerceRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5f0ca3265204bfa7f8afaf0321046bb04b97f36d
01nevida <01@vida>
2023-10-14 17:28:56 -0300

    criada a entidade Category

--- src/main/java/com/example/ecommerce_restapi/ECommerceRestApiApplication.java
+++ src/main/java/com/example/ecommerce_restapi/ECommerceRestApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ECommerceRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommerceRestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceRestApiApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/ecommerce_restapi/models/Category.java
+++ src/main/java/com/example/ecommerce_restapi/models/Category.java
@@ -1,2 +1,20 @@
-package com.example.ecommerce_restapi.models;public class Product {
+package com.example.ecommerce_restapi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String name;
 }

9a097d99b351ab030c0a147b9e57b7c0abba90d6
01nevida <01@vida>
2023-10-21 11:45:16 -0300

    Criada a relacao entre produtos e categorias

--- src/main/java/com/example/ecommerce_restapi/models/Product.java
+++ src/main/java/com/example/ecommerce_restapi/models/Product.java
@@ -1,36 +1,33 @@
 package com.example.ecommerce_restapi.models;
-
 import jakarta.persistence.*;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Getter
 @NoArgsConstructor
-@Table(name = "products")
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(columnDefinition = "TEXT")
     private String description;
     private Double price;
+    @Column(columnDefinition = "TEXT")
     private String imgUrl;
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant date;
     @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns= @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+    public Product(Long id, String name, String description, Double price, String urlImg) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
-        this.imgUrl = imgUrl;
-        this.date = date;
+        this.imgUrl = urlImg;
     }
 }

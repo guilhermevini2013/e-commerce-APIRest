c830decf8eefa59d565daef008d38405cd46fa25
01nevida <01@vida>
2023-11-19 18:51:59 -0300

    criado od mapeamentos de classe User e Role.java

--- src/main/java/com/example/ecommerce_restapi/models/Role.java
+++ src/main/java/com/example/ecommerce_restapi/models/Role.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce_restapi.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tb_role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
--- src/main/java/com/example/ecommerce_restapi/models/User.java
+++ src/main/java/com/example/ecommerce_restapi/models/User.java
@@ -1,6 +1,6 @@
 package com.example.ecommerce_restapi.models;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +11,19 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
-    @ManyToOne
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 }
